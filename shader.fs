c173737892d690f8ce37baf7e1d800b75d9faa8c
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-03-26 13:36:26 +1100

    make width/height a uniform instead of a const

--- shader.fs
+++ shader.fs
@@ -4,18 +4,15 @@ in vec2 fragTexCoord;
 
 out vec4 finalColor;
 
+// The bottleneck, we cant have to many points, the shader cant handle it.
 #define MAX_POINTS 256
 
 uniform int num_points;
 uniform vec2 points[MAX_POINTS];
 uniform int colors[MAX_POINTS]; // packed RGBA color (an [r, g, b, a] array)
 
-const float width  = 1600;
-const float height =  900;
-
-float dist_sqr(in vec2 a, in vec2 b) {
-    return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
-}
+uniform int width;
+uniform int height;
 
 void main() {
     if (num_points > MAX_POINTS) {
@@ -33,7 +30,6 @@ void main() {
     int close_index = 0;
     float d1 = length(pos - points[0]);
     for (int i = 0; i < num_points; i++) {
-
         float d2 = length(pos - points[i]);
         if (d2 < d1) {
             d1 = d2;
@@ -41,7 +37,7 @@ void main() {
         }
     }
 
-    int  color = colors[close_index];
+    int color = colors[close_index];
 
     float r = ((color >> 0*8) & 0xFF) / 255.0;
     float g = ((color >> 1*8) & 0xFF) / 255.0;
--- voronoi_shader.c
+++ voronoi_shader.c
@@ -18,6 +18,10 @@ int num_points_loc;
 int points_loc;
 int colors_loc;
 
+int width_loc;
+int height_loc;
+
+
 #define SHADER_PATH "shader.fs"
 
 void init_voronoi(void) {
@@ -27,9 +31,15 @@ void init_voronoi(void) {
     points_loc     = GetShaderLocation(shader, "points");
     colors_loc     = GetShaderLocation(shader, "colors");
 
+    width_loc      = GetShaderLocation(shader, "width");
+    height_loc     = GetShaderLocation(shader, "height");
+
     if (num_points_loc != -1) fprintf(stderr, "WARNING: 'num_points_loc' was not set\n");
     if (points_loc     != -1) fprintf(stderr, "WARNING: 'points_loc' was not set\n");
     if (colors_loc     != -1) fprintf(stderr, "WARNING: 'colors_loc' was not set\n");
+
+    if (width_loc      != -1) fprintf(stderr, "WARNING: 'width_loc'  was not set\n");
+    if (height_loc     != -1) fprintf(stderr, "WARNING: 'height_loc' was not set\n");
 
     small_buffer = LoadRenderTexture(1, 1);
 }
@@ -41,17 +51,14 @@ void finish_voronoi(void) {
 
 
 void draw_voronoi(RenderTexture2D target, Vector2 *points, Color *colors, size_t num_points) {
+    PROFILER_ZONE("Setup shader");
+    if (num_points_loc != -1) SetShaderValue(shader, num_points_loc, &num_points, SHADER_UNIFORM_INT);
+    if (points_loc != -1) SetShaderValueV(shader, points_loc, points, SHADER_UNIFORM_VEC2, num_points);
+    if (colors_loc != -1) SetShaderValueV(shader, colors_loc, colors, SHADER_UNIFORM_INT, num_points);
 
-    if (num_points_loc != -1) {
-        SetShaderValue(shader, num_points_loc, &num_points, SHADER_UNIFORM_INT);
-    }
-    if (points_loc != -1) {
-        SetShaderValueV(shader, points_loc, points, SHADER_UNIFORM_VEC2, num_points);
-    }
-    if (colors_loc != -1) {
-        SetShaderValueV(shader, colors_loc, colors, SHADER_UNIFORM_INT, num_points);
-    }
-
+    if (width_loc  != -1) SetShaderValue(shader, width_loc,  &target.texture.width,  SHADER_UNIFORM_INT);
+    if (height_loc != -1) SetShaderValue(shader, height_loc, &target.texture.height, SHADER_UNIFORM_INT);
+    PROFILER_ZONE_END();
 
     PROFILER_ZONE("Use shader");
     BeginTextureMode(target);
@@ -66,7 +73,6 @@ void draw_voronoi(RenderTexture2D target, Vector2 *points, Color *colors, size_t
                 WHITE
             );
         EndShaderMode();
-
     EndTextureMode();
     PROFILER_ZONE_END();
 }
