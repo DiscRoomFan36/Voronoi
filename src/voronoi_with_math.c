acdbafd3100409eccdb19d40b6529b246c49126c
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-04-02 21:42:06 +1100

    increased percisiton, problem still persists...

--- src/voronoi_with_math.c
+++ src/voronoi_with_math.c
@@ -30,9 +30,14 @@
 #define SWAP(a, b) do {typeof(a) tmp = a; a = b; b = tmp;} while(0)
 
 
+typedef struct DoubleVector2 {
+    double x, y;
+} DoubleVector2;
+
+
 typedef struct Polygon {
     // the points of the polygon
-    Vector2 *items;
+    DoubleVector2 *items;
     size_t count;
     size_t capacity;
 } Polygon;
@@ -43,9 +48,9 @@ typedef struct Polygon {
 // flip height, if not zero, flip vertical
 void draw_polygon(Polygon polygon, Color color, int flip_height) {
     for (size_t i = 1; i < polygon.count - 1; i++) {
-        Vector2 v1 = polygon.items[0];
-        Vector2 v2 = polygon.items[i];
-        Vector2 v3 = polygon.items[i+1];
+        Vector2 v1 = {polygon.items[0  ].x, polygon.items[0  ].y};
+        Vector2 v2 = {polygon.items[i  ].x, polygon.items[i  ].y};
+        Vector2 v3 = {polygon.items[i+1].x, polygon.items[i+1].y};
 
         if (flip_height) {
             v1.y = flip_height - v1.y;
@@ -68,12 +73,12 @@ void draw_polygon(Polygon polygon, Color color, int flip_height) {
 #define FINF 999999999.0f
 
 // https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
-Vector2 line_line_intersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4) {
+DoubleVector2 line_line_intersection(DoubleVector2 p1, DoubleVector2 p2, DoubleVector2 p3, DoubleVector2 p4) {
     float w = (p1.x - p2.x)*(p3.y - p4.y) - (p1.y - p2.y)*(p3.x - p4.x);
 
     if (w == 0) {
         // parallel or coincident, return junk number
-        return (Vector2){FINF, FINF};
+        return (DoubleVector2){FINF, FINF};
     }
 
     float d_x = (p1.x*p2.y - p1.y*p2.x)*(p3.x - p4.x) - (p1.x - p2.x)*(p3.x*p4.y - p3.y*p4.x);
@@ -83,12 +88,12 @@ Vector2 line_line_intersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4) {
     float i_x = d_x / w;
     float i_y = d_y / w;
 
-    return (Vector2){i_x, i_y};
+    return (DoubleVector2){i_x, i_y};
 }
 
 // check if two line points, are intersected with a point
 // that is known to intersect with it.
-bool intersection_point_intersects(Vector2 p1, Vector2 p2, Vector2 intersect) {
+bool intersection_point_intersects(DoubleVector2 p1, DoubleVector2 p2, DoubleVector2 intersect) {
     if (p1.x == p2.x) {
         // vertical line
 
@@ -132,7 +137,7 @@ bool intersection_point_intersects(Vector2 p1, Vector2 p2, Vector2 intersect) {
 }
 
 // https://stackoverflow.com/questions/1560492/how-to-tell-whether-a-point-is-to-the-right-or-left-side-of-a-line
-bool isLeft(Vector2 a, Vector2 b, Vector2 c) {
+bool isLeft(DoubleVector2 a, DoubleVector2 b, DoubleVector2 c) {
     return (b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x) > 0;
 }
 
@@ -142,18 +147,24 @@ Polygon polygon;
 Polygon tmp_poly1;
 Polygon tmp_poly2;
 
+Polygon points_double = {0};
+
 
 void init_voronoi(void) {
     // clear the polygon's
     polygon   = (Polygon){0};
     tmp_poly1 = (Polygon){0};
     tmp_poly2 = (Polygon){0};
+
+    points_double = (Polygon){0};
 }
 
 void finish_voronoi(void) {
     da_free(&polygon);
     da_free(&tmp_poly1);
     da_free(&tmp_poly2);
+
+    da_free(&points_double);
 }
 
 
@@ -223,48 +234,54 @@ void draw_voronoi(RenderTexture2D target, Vector2 *points, Color *colors, size_t
     // will also be acceptable to.), a 5x-6x speedup is easily possible.
 
 
+    // useing this polygon as a double vector2 array
+    points_double.count = 0;
+    for (size_t i = 0; i < num_points; i++) {
+        da_append(&points_double, ((DoubleVector2){(double)points[i].x, (double)points[i].y}));
+    }
+
 
     for (size_t point_index = 0; point_index < num_points; point_index++) {
         // 1. Get a point.
-        Vector2 point = points[point_index];
+        DoubleVector2 point = points_double.items[point_index];
 
         // 2. Construct a polygon that fills the screen
         polygon.count = 0;
-        da_append(&polygon, ((Vector2){    0,      0}));
-        da_append(&polygon, ((Vector2){width,      0}));
-        da_append(&polygon, ((Vector2){width, height}));
-        da_append(&polygon, ((Vector2){    0, height}));
+        da_append(&polygon, ((DoubleVector2){    0,      0}));
+        da_append(&polygon, ((DoubleVector2){width,      0}));
+        da_append(&polygon, ((DoubleVector2){width, height}));
+        da_append(&polygon, ((DoubleVector2){    0, height}));
 
 
         // 3. For every other point:
         for (size_t other_point_index = 0; other_point_index < num_points; other_point_index++) {
             if (other_point_index == point_index) continue;
 
-            Vector2 other_point = points[other_point_index];
+            DoubleVector2 other_point = points_double.items[other_point_index];
 
             // 4. Find the mid line parallel to those point
 
             // the points in here form a perpendicular line.
-            Vector2 perpendicular_points[2];
+            DoubleVector2 perpendicular_points[2];
             {
-                Vector2 p1 = point;
-                Vector2 p2 = other_point;
+                DoubleVector2 p1 = point;
+                DoubleVector2 p2 = other_point;
 
                 // mid point
                 // m = (p1 + p2) / 2
-                Vector2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+                DoubleVector2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
 
                 // direction vector
                 // v = p2 - p1
-                Vector2 v = Vector2Subtract(p2, p1);
+                DoubleVector2 v = {p2.x - p1.x, p2.y - p1.y}; // Vector2Subtract(p2, p1);
 
                 // rotated 90 direction vector
                 // v1 = (-v.y, v.x)
-                Vector2 v_1 = {-v.y, v.x};
+                DoubleVector2 v_1 = {-v.y, v.x};
 
                 // add to the mid point
                 // m1 = m + v1
-                Vector2 m_1 = Vector2Add(m, v_1);
+                DoubleVector2 m_1 = {m.x + v_1.x, m.y + v_1.y}; // Vector2Add(m, v_1);
 
                 perpendicular_points[0] = m;
                 perpendicular_points[1] = m_1;
@@ -272,21 +289,21 @@ void draw_voronoi(RenderTexture2D target, Vector2 *points, Color *colors, size_t
 
 
             // 5. Cut the polygon and keep the side that is close to the original point
-            Vector2 intersection_points[2];
+            DoubleVector2 intersection_points[2];
             size_t intersection_points_i[2];
             size_t intersection_points_count = 0;
 
             // loop over all edges
             for (size_t i = 0; i < polygon.count; i++) {
-                Vector2 p1 = polygon.items[i];
-                Vector2 p2 = polygon.items[(i+1)%polygon.count];
+                DoubleVector2 p1 = polygon.items[i];
+                DoubleVector2 p2 = polygon.items[(i+1)%polygon.count];
 
                 // perpendicular points.
-                Vector2 p3 = perpendicular_points[0];
-                Vector2 p4 = perpendicular_points[1];
+                DoubleVector2 p3 = perpendicular_points[0];
+                DoubleVector2 p4 = perpendicular_points[1];
 
                 // get intersection
-                Vector2 intersect = line_line_intersection(p1, p2, p3, p4);
+                DoubleVector2 intersect = line_line_intersection(p1, p2, p3, p4);
 
                 // if the line segment intersects with the intersect point, add it to the intersection array.
                 if (intersection_point_intersects(p1, p2, intersect)) {
@@ -345,7 +362,7 @@ void draw_voronoi(RenderTexture2D target, Vector2 *points, Color *colors, size_t
 
                 // and add the second intersection the the first polygon
                 da_append(&tmp_poly1, intersection_points[1]);
-                // finnaly add the rest to the first polygon
+                // finally add the rest to the first polygon
                 index++;
                 while (index < polygon.count) {
                     da_append(&tmp_poly1, polygon.items[index]);
